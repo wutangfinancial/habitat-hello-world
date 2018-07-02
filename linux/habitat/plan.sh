0765f999f14a121b125b07c6b03cc57f57148af4
Adam Salowitz <asalowi1@ford.com>
2018-07-02 15:29:01 -0400

    refactor for linux

--- linux/habitat/plan.sh
+++ linux/habitat/plan.sh
@@ -1,8 +1,12 @@
 pkg_name=habitat-hello-world
 pkg_origin=asalowi1
-pkg_version="0.1.0"
+pkg_version="0.2.0"
 pkg_maintainer="Adam Salowitz <asalowi1@ford.com>"
 pkg_license=("Apache-2.0")
+pkg_deps=(core/bash)
+pkg_svc_user="root"
+pkg_svc_group="root"
+
 # If your package does NOT download and install a pkg_source you need to override
 # build and install functions.  See https://www.habitat.sh/docs/best-practices/#binary-wrapper
 do_build() {
@@ -10,6 +14,6 @@ do_build() {
 }
 
 do_install() {
+  cp -r hello-world.sh $pkg_prefix
   chmod +x $pkg_prefix/hello-world.sh
-  $pkg_prefix/hello-world.sh
 }
