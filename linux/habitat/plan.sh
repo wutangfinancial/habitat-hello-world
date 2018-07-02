a01953e132cc2e25a27101bcb54bdc518d539e0e
Adam Salowitz <asalowi1@ford.com>
2018-07-02 17:04:02 -0400

    removed health_check for now

--- linux/habitat/plan.sh
+++ linux/habitat/plan.sh
@@ -1,10 +1,11 @@
 pkg_name=habitat-hello-world
 pkg_origin=asalowi1
-pkg_version="0.2.3"
+pkg_version="0.2.5"
 pkg_maintainer="Adam Salowitz <asalowi1@ford.com>"
 pkg_license=("Apache-2.0")
 pkg_deps=(core/bash)
 pkg_interpreters=(bin/bash)
+pkg_bin_dirs=(bin)
 
 # If your package does NOT download and install a pkg_source you need to override
 # build and install functions.  See https://www.habitat.sh/docs/best-practices/#binary-wrapper
@@ -13,6 +14,5 @@ do_build() {
 }
 
 do_install() {
-  cp -r hello-world.sh $pkg_prefix
-  chmod +x $pkg_prefix/hello-world.sh
+  install -D hello-world.sh "${pkg_prefix}/bin/hello-world.sh"
 }
